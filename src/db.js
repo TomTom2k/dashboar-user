51a81c09b84f646e883a18b36f6b428c33cd81d3
Nguyễn Thành Tín <88455780+TomTom2k@users.noreply.github.com>
2024-07-09 01:28:31 +0700

    fix: upload connect db

--- src/db.js
+++ src/db.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
 	try {
 		await mongoose.connect(
-			'mongodb+srv://ngthanhtin68:Tin1842003%40@cluster0.teb8bvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/dashboard'
+			'mongodb+srv://ngthanhtin68:Tin1842003%40cluster0.teb8bvv.mongodb.net/mydatabase?retryWrites=true&w=majority',
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}
 		);
 		console.log('MongoDB connected...');
 	} catch (err) {
